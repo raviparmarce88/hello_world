a447f8c9c22e0cf795be04bfac98447567743352
Ravi Parmar <raviparmar.ce88@gmail.com>
2015-06-21 17:46:34 +0530

    3rd commit

--- HelloWorld_Git/src/HelloWorld.java
+++ HelloWorld_Git/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
 		System.out.println("Hello Git 1st commit");
+		System.out.println("Hello Git 2nd Commit after cloned");
 	}
 
 }
